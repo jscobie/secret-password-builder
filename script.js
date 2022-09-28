42979769e3ce8c8eed0d6eeafcf655de8fea1320
Jacob Scobie <jscobie@focus-solutions.net>
2022-09-27 21:24:31 -0400

    Late night progress commit

--- script.js
+++ script.js
@@ -38,12 +38,83 @@ function generatePassword() {
     const isNumeric = confirm("Include Numbers?");
     //get user input - special characters
     const isSpecial = confirm("Include Special Characters?");
+    
+    if (isLowercase && isUppercase && isNumeric && isSpecial) {
+    // 4 selected build possible character from all 4 lists
+      var passwordCharacters = lcaseLetters.concat(ucaseLetters, numbers, specialCharacters);
+    // 3 selected build possible character from 3 selected lists
+    } else if (isLowercase && isUppercase && isNumeric) {
+      var passwordCharacters = lcaseLetters.concat(ucaseLetters, numbers);
+    } else if (isLowercase && isUppercase && isSpecial) {
+      var passwordCharacters = lcaseLetters.concat(ucaseLetters, specialCharacters);
+    } else if (isLowercase && isNumeric && isSpecial) {
+      var passwordCharacters = lcaseLetters.concat(numbers, specialCharacters);
+    } else if (isUppercase && isNumeric && isSpecial) {
+      var passwordCharacters =ucaseLetters.concat(numbers, specialCharacters);
+    // 2 selected - build possible character from 2 selected lists
+    } else if (isLowercase && isUppercase) {
+      var passwordCharacters = [lcaseLetters] + [ucaseLetters];
+    } else if (isLowercase && isNumeric) {
+      var passwordCharacters = [lcaseLetters] + [numbers];
+    } else if (isLowercase && isSpecial) {
+      var passwordCharacters = [lcaseLetters] + [specialCharacters];
+    } else if (isUppercase && isNumeric) {
+      var passwordCharacters = [ucaseLetters] + [numbers];
+    } else if (isUppercase && isSpecial) {
+      var passwordCharacters = [ucaseLetters] + [specialCharacters];
+    } else if (isLowercase && isUppercase && isNumeric && isSpecial) {
+      var passwordCharacters = [lcaseLetters] + [ucaseLetters] + [numbers] + [specialCharacters];
+    } else if (isNumeric && isSpecial) {
+      var passwordCharacters = [numbers] + [specialCharacters];
+      // 1 selected - build possible characters from only the selected list
+    } else if (isLowercase) {
+      var passwordCharacters =[lcaseLetters];
+    } else if (isUppercase) {
+      var passwordCharacters =[ucaseLetters];
+    } else if (isNumeric) {
+      var passwordCharacters =[numbers];
+    } else if (isSpecial) {
+    var passwordCharacters =[specialCharacters];
+    // 0 selected - show error
+    } else {
+      alert("You Selected all Cancel/No  Action Aborted")
+      let password = "ERROR - NO PASSWORD GENERATED"
+      return password;
+    }
 
-    // console.log(isLowercase, isUppercase, isNumeric, isSpecial)
-    let password = [isLowercase, isUppercase, isNumeric, isSpecial];
+
+    // shuffle array using Fisher-Yates aka Knuth Shuffle CREDIT: https://bost.ocks.org/mike/shuffle/
+    // function shuffle(array) {
+    //   let currentIndex = array.length,  randomIndex;
+    
+    //   // While there remain elements to shuffle.
+    //   while (currentIndex != 0) {
+    
+    //     // Pick a remaining element.
+    //     randomIndex = Math.floor(Math.random() * currentIndex);
+    //     currentIndex--;
+    
+    //     // And swap it with the current element.
+    //     [array[currentIndex], array[randomIndex]] = [
+    //       array[randomIndex], array[currentIndex]];
+    //   }
+    
+    //   return array;
+    // }
+    
+    // Used like so
+    // var arr = [2, 11, 37, 42];
+    // shuffle(arr);
+    // console.log(arr);
+
+    // TODO:
+    // randomly select characters from password characters
+    
+    let password = passwordCharacters
     return password;
   }
 
 }
 
 
+ 
